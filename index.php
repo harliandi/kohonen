81f3bdae0655064dd32d3b0ff506a128fea15b1d
Harliandi <harliandi2704@gmail.com>
2017-11-19 19:54:47 +0700

    done

--- index.php
+++ index.php
@@ -14,7 +14,6 @@
     //todo: modifikasi bobot
     //todo: modifikasi laju pemahaman
     //todo: uji kondisi penghentian
-    //todo: uji kondisi penghentian
 
     //data vektor
     $v = [
@@ -24,7 +23,7 @@
         3 => [0, 0, 1, 1]
     ];
 
-    //laju pemahaman
+    //laju pemahaman (learning rate)
     $index_alpha = 0;
     $alpha[$index_alpha] = 0.6;
 
@@ -40,14 +39,14 @@
     //jumlah grup
     $grup = 2;
 
-    //variabel penghentian
-    $stop = false;
+    //max iterasi
+    $max_epoch = 10;
 
     //variabel sementara untuk menampung bobot terakhir
     $print_bobot = '';
 
     //kondisi penghentian
-    while ($stop === false) {
+    for ($iterasi = 1; $iterasi <= $max_epoch; $iterasi++) {
         //perhitungan distance untuk masing - masing vektor dan perubahan bobot
         for ($i = 0; $i < count($v); $i++) {
             //menghitung distance per vektor
@@ -77,12 +76,37 @@
         //modifikasi laju pemahaman
         $index_alpha += 1;
         $alpha[$index_alpha] = 0.5 * $alpha[$index_alpha - 1];
+
     }
 
     echo '<pre>';
-    echo 'Lanjutan';
-    echo '<br>';
-    print_r($print_bobot);
-    echo '<br>';
+    //cetak bobot optimal
     print_r($w);
+    echo '<br>';
+    print_r($v);
+    echo '<br>';
+
+    //bobot optimal didapat, hitung vektor masuk ke grup mana dengan bobot optimal
+    for ($i = 0; $i < count($v); $i++) {
+        //menghitung distance per vektor
+        for ($j = 1; $j <= $grup; $j++) {
+            $q1 = pow($w[$j][0] - $v[$i][0], 2);
+            $q2 = pow($w[$j][1] - $v[$i][1], 2);
+            $q3 = pow($w[$j][2] - $v[$i][2], 2);
+            $q4 = pow($w[$j][3] - $v[$i][3], 2);
+            $distance[$j] = abs($q1 + $q2 + $q3 + $q4);
+        }
+
+        //pengecekan distance vektor terkecil
+        if ($distance[1] > $distance[2]) {
+            $d = 2;
+        } else {
+            $d = 1;
+        }
+
+        //hasil
+        echo 'Vektor ke - ' . $i . ' berada di cluster ' . $d;
+        echo '<br>';
+    }
+
     echo '</pre>';
